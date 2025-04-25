e782ac3c7775febb78fe9a69371b3824e9fb6fe8
saksham angane <107934960+saksham77ang@users.noreply.github.com>
2025-04-25 08:31:54 +0530

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,16 +10,12 @@ const work = defineCollection({
   }),
 })
 
-const blog = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    summary: z.string(),
-    date: z.coerce.date(),
-    tags: z.array(z.string()),
-    draft: z.boolean().optional(),
-  }),
-})
+#const blog = defineCollection({
+  #type: "content",
+  #schema: z.object({
+    #title: z.string(),
+    #summary: z.string(),
+    
 
 const projects = defineCollection({
   type: "content",
